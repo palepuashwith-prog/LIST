065d1565a25397225ae075252a2e29dac504f389
palepuashwith-prog <palepuashwith@gmail.com>
2025-08-20 11:15:48 +0530

    Add files via upload

---
+++ REPLACE AND SEARCH.py
@@ -0,0 +1,12 @@
+list=['red','blue','black','yellow','pink','green']
+search_word="green"
+replace_word="white"
+for i in range(len(list)):
+  if list[i]==search_word:
+      list[i]=replace_word
+print(list)
+ 
+ 
+ 
+ 
+ 
